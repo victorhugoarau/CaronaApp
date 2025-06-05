e5c6e35b0c9d47ba5fd3073e5fb77d29e48a70c3
Choquinn <Voltzchoquinn@gmail.com>
2025-06-05 13:35:56 -0300

    felipe/victorhugo/app

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -26,6 +26,8 @@ export default function Layout() {
                 }}
             />
             
+
+            
             <Tabs.Screen
                 name="Rotinas"
                 options={{
